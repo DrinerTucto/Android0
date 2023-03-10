7a8289d6745f69c05702e9d1a18d382588bb3754
DrinerTucto <1304745@senati.pe>
2023-03-09 21:06:03 -0500

    Avance

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText p1;
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.textView);
+        p1 = (EditText) findViewById(R.id.nombre);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        String n1;
+
+
+    txt.setText("Hola mundo");
     }
 }
+
